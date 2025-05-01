df6eaef9221159d60a014b6722939daeaa1ba69f
kRamu581 <kanamramu18@gmail.com>
2025-05-01 08:40:34 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CSS Styling Methods</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <style>
+    /* Internal CSS */
+    .internal-style {
+      color: blue;
+      font-size: 20px;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+
+  <h1 style="color: red; font-size: 24px; text-align: center;">This is Inline CSS</h1>
+
+  <p class="internal-style">This is styled using Internal CSS.</p>
+
+  <div class="external-style">This is styled using External CSS.</div>
+
+</body>
+</html>
