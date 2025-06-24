7752b22d4195b2f1d31557ca3a9de1209efca36b
K Ramu <kanamramu18@gmail.com>
2025-06-24 23:11:17 +0530

    Create 6(a)nestedIf.sql

---
+++ dbms/6(a)nestedIf.sql
@@ -0,0 +1,21 @@
+DECLARE
+  a NUMBER := 10;
+  b NUMBER := 20;
+  c NUMBER := 30;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('a'|| a || ' b'|| b || ' c'|| c);
+
+  IF a > b AND a > c THEN
+    DBMS_OUTPUT.PUT_LINE('a is greater');
+  ELSE
+    IF b > a AND b > c THEN
+      DBMS_OUTPUT.PUT_LINE('b is greater');
+    ELSE
+      DBMS_OUTPUT.PUT_LINE('c is greater');
+    END IF;
+  END IF;
+END;
+/
+--Output:
+--a10 b20 c30
+--c is greater
