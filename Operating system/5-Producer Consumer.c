901bc27aa79457fbdc340c6753faa30bccc4349d
K Ramu <kanamramu18@gmail.com>
2025-06-25 19:22:19 +0530

    Update 5-Producer Consumer.c

--- Operating system/5-Producer Consumer.c
+++ Operating system/5-Producer Consumer.c
@@ -1,17 +1,11 @@
-// Program to simulate Producer-Consumer Problem using Semaphores
-// Author: [Your Name]
-// Date: [Exam Date]
-
 #include <stdio.h>
 #include <stdlib.h>
 
-// Simulated semaphores and buffer variables
 int mutex = 1;
 int full = 0;
 int empty = 10;
 int x = 0;
 
-// Function to simulate producer
 void producer() {
     --mutex;
     ++full;
@@ -21,7 +15,6 @@ void producer() {
     ++mutex;
 }
 
-// Function to simulate consumer
 void consumer() {
     --mutex;
     --full;
@@ -31,42 +24,37 @@ void consumer() {
     ++mutex;
 }
 
-// Main function with menu
 int main() {
-    int choice;
+    int n;
 
-    printf("Producer-Consumer Problem using Semaphores (Simulated)\n");
+    printf("\n1. Press 1 for Producer");
+    printf("\n2. Press 2 for Consumer");
+    printf("\n3. Press 3 for Exit");
 
     while (1) {
-        printf("\n\n1. Press 1 to Produce");
-        printf("\n2. Press 2 to Consume");
-        printf("\n3. Press 3 to Exit");
-        printf("\nEnter your choice: ");
-        scanf("%d", &choice);
+        printf("\n\nEnter your choice: ");
+        scanf("%d", &n);
 
-        switch (choice) {
+        switch (n) {
             case 1:
-                if (mutex == 1 && empty != 0) {
+                if (mutex == 1 && empty != 0)
                     producer();
-                } else {
+                else
                     printf("\nBuffer is full!");
-                }
                 break;
 
             case 2:
-                if (mutex == 1 && full != 0) {
+                if (mutex == 1 && full != 0)
                     consumer();
-                } else {
+                else
                     printf("\nBuffer is empty!");
-                }
                 break;
 
             case 3:
                 exit(0);
-                break;
 
             default:
-                printf("\nInvalid choice! Please try again.");
+                printf("\nInvalid choice!");
         }
     }
 
